ff9db26f0b01014008a1b024114acb631619638c
Gian Biondi <gian@namely.com>
2015-02-24 12:58:43 -0500

    Range downloading freaking works

--- main.go
+++ main.go
@@ -20,12 +20,6 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	outFile, err := os.Create("vimeofile.mp4")
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	defer outFile.Close()
 	if resp.Header["Accept-Ranges"][0] == "bytes" {
 		var wg sync.WaitGroup
 		log.Println("Ranges Supported!")
@@ -37,19 +31,21 @@ func main() {
 		var end_byte int
 		start_byte := 0
 		for i := 0; i < threads; i++ {
+			filename := "vimeo.part." + strconv.Itoa(i)
 			wg.Add(1)
 			//start_byte := i * int(calculated_chunksize)
 			end_byte = start_byte + int(calculated_chunksize)
 			log.Println("Dispatch ", start_byte, " to ", end_byte)
-			go fetchChunk(int64(start_byte), int64(end_byte), url, outFile, &wg)
+			go fetchChunk(int64(start_byte), int64(end_byte), url, filename, &wg)
 			start_byte = end_byte
 		}
 		if end_byte < content_size {
 			wg.Add(1)
 			start_byte = end_byte
 			end_byte = content_size
+			filename := "vimeo.part." + strconv.Itoa(threads)
 			log.Println("Dispatch ", start_byte, " to ", end_byte)
-			go fetchChunk(int64(start_byte), int64(end_byte), url, outFile, &wg)
+			go fetchChunk(int64(start_byte), int64(end_byte), url, filename, &wg)
 		}
 		/*
 			for i := 0; i < content_size; {
@@ -65,7 +61,7 @@ func main() {
 	}
 }
 
-func fetchChunk(start_byte, end_byte int64, url string, file *os.File, wg *sync.WaitGroup) {
+func fetchChunk(start_byte, end_byte int64, url string, filename string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	//log.Println("Downloading byte ", start_byte)
 	client := new(http.Client)
@@ -74,7 +70,8 @@ func fetchChunk(start_byte, end_byte int64, url string, file *os.File, wg *sync.
 		log.Fatal(err)
 		return
 	}
-	req.Header.Set("Range: ", fmt.Sprintf("bytes=%d-%d", start_byte, end_byte-1))
+	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", start_byte, end_byte-1))
+	log.Println(req.Header)
 	res, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
@@ -86,7 +83,14 @@ func fetchChunk(start_byte, end_byte int64, url string, file *os.File, wg *sync.
 		log.Fatal(err)
 		return
 	}
-	file.WriteAt(body, start_byte)
+	outfile, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer outfile.Close()
+	outfile.Write(body)
+	//file.WriteAt(body, start_byte)
 	log.Println("Finished Downloading byte ", start_byte)
 	return
 }
