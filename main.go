566aaa4b6373b2749c074ac715464cb75572448f
Gian Biondi <gian@namely.com>
2015-02-24 12:29:52 -0500

    Parallel downloads initiate

--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
 )
 
 const url = "http://storage.googleapis.com/vimeo-test/work-at-vimeo.mp4"
-const chunksize = 30000
+const chunksize = 100000
+const threads = 30
 
 func main() {
 	resp, err := http.Get(url)
@@ -25,21 +27,47 @@ func main() {
 	}
 	defer outFile.Close()
 	if resp.Header["Accept-Ranges"][0] == "bytes" {
+		var wg sync.WaitGroup
 		log.Println("Ranges Supported!")
 		log.Println("Content Size:", resp.Header["Content-Length"][0])
-		//content_size, _ := strconv.ParseInt(resp.Header["Content-Length"][0], 0, 16)
 		content_size, _ := strconv.Atoi(resp.Header["Content-Length"][0])
-		for i := 0; i < content_size; {
-			end_byte := i + chunksize
-			go fetchChunk(int64(i), int64(end_byte), url, outFile)
-			i = end_byte
+		//calculated_chunksize := math.Ceil(float64(content_size) / threads)
+		calculated_chunksize := content_size / threads
+		log.Println("Chunk Size: ", int(calculated_chunksize))
+		var end_byte int
+		start_byte := 0
+		for i := 0; i < threads; i++ {
+			wg.Add(1)
+			//start_byte := i * int(calculated_chunksize)
+			end_byte = start_byte + int(calculated_chunksize)
+			log.Println("Dispatch ", start_byte, " to ", end_byte)
+			go fetchChunk(int64(start_byte), int64(end_byte), url, outFile, &wg)
+			start_byte = end_byte
 		}
+		if end_byte < content_size {
+			wg.Add(1)
+			start_byte = end_byte
+			end_byte = content_size
+			log.Println("Dispatch ", start_byte, " to ", end_byte)
+			go fetchChunk(int64(start_byte), int64(end_byte), url, outFile, &wg)
+		}
+		/*
+			for i := 0; i < content_size; {
+				wg.Add(1)
+				end_byte := i + chunksize
+				go fetchChunk(int64(i), int64(end_byte), url, outFile, &wg)
+				i = end_byte
+			}
+		*/
+		wg.Wait()
+		log.Println("Download Complete!")
 		return
 	}
 }
 
-func fetchChunk(start_byte, end_byte int64, url string, file *os.File) {
-	log.Println("Downloading byte ", start_byte)
+func fetchChunk(start_byte, end_byte int64, url string, file *os.File, wg *sync.WaitGroup) {
+	defer wg.Done()
+	//log.Println("Downloading byte ", start_byte)
 	client := new(http.Client)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -59,5 +87,6 @@ func fetchChunk(start_byte, end_byte int64, url string, file *os.File) {
 		return
 	}
 	file.WriteAt(body, start_byte)
+	log.Println("Finished Downloading byte ", start_byte)
 	return
 }
